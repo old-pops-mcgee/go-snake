c9d1d8f7dda6128f67b675fdea2ee7fa01023ac8
old-pops-mcgee <oldpopsmcgeegaming@gmail.com>
2025-06-14 12:08:40 -0400

    Window opening

--- main.go
+++ main.go
@@ -4,5 +4,16 @@ import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
 	rl.InitWindow(800, 450, "Go Snake")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawText("Hello, World!", 200, 200, 20, rl.Gray)
+		rl.EndDrawing()
+	}
 
 }
